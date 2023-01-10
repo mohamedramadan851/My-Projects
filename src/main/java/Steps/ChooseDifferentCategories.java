4c938c4ded25e5450713e868f492793813488b35
mohamedramadan851 <116560799+mohamedramadan851@users.noreply.github.com>
2023-01-10 10:10:28 +0200

    Add files via upload

---
+++ src/main/java/Steps/ChooseDifferentCategories.java
@@ -0,0 +1,29 @@
+package Steps;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Then;
+import org.testng.Assert;
+import pages.HomePage;
+
+import static Steps.Hooks.driver;
+
+public class ChooseDifferentCategories {
+    HomePage homePage;
+
+    @And("user hover on electronic category")
+    public void userHoverOnElectronicCategory() {
+        homePage = new HomePage(driver);
+        homePage.hoverOnElectronicsCat();
+    }
+
+    @And("user click on cellPhone category")
+    public void userClickOnCellPhoneCategory() {
+        homePage.clickOnCellPhoneCat();
+    }
+
+    @Then("cell phones page should appear to the user")
+    public void cellPhonesAppear() {
+        String expectedUrl = "https://demo.nopcommerce.com/cell-phones";
+        Assert.assertEquals(driver.getCurrentUrl(), expectedUrl);
+    }
+}
